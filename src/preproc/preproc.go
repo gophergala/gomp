7089dd68c6dae0c19a5af57de60f79c173d70dd9
Yuri Gorshenin <mipt.vi002@gmail.com>
2015-01-24 11:41:36 +0300

    Added stub implementation of preproc.

---
+++ src/preproc/preproc.go
@@ -0,0 +1,11 @@
+package preproc
+
+// Run preprocessor on a source. filename is used for error reporting.
+// This function is currently not implemented.
+func PreprocFile(source, filename string) (result string, err error) {
+	return
+}
+
+func PreprocFileImpl(source, filename string) (result string, err error) {
+	return
+}
