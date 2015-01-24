ebc4f1d06499425308b351e1dc08697d5f36c16a
Aleksandr Shevchenko <aleksandr.shevchenko@phystech.edu>
2015-01-24 15:41:23 +0400

    goimport-ed gompp.go

--- gompp/gompp.go
+++ gompp/gompp.go
@@ -4,17 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+
 	"github.com/gophergala/gomp/preproc"
 )
 
 func main() {
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan() // Read one line
-		result, err := preproc.PreprocFile(scanner.Text(), "stdin")
-		if (err != nil) {
-			fmt.Fprintln(os.Stderr, "Gompp error while using preproc.PreprocFile:\n", err.Error())
-			os.Exit(-1)
-		}
-		fmt.Fprintf(os.Stdin, result)
-		return
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan() // Read one line
+	result, err := preproc.PreprocFile(scanner.Text(), "stdin")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Gompp error while using preproc.PreprocFile:\n", err.Error())
+		os.Exit(-1)
+	}
+	fmt.Fprintf(os.Stdin, result)
+	return
 }
