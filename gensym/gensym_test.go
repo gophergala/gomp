ef5fbebe36120c4029d8c872f4de167465e6be16
max <max@>
2015-01-24 23:26:17 +0300

    gensym: added a test

--- gensym/gensym_test.go
+++ gensym/gensym_test.go
@@ -1,8 +1,8 @@
 package gensym_test
 
 import (
-	. "github.com/gophergala/gomp/gensym"
 	"testing"
+	. "github.com/gophergala/gomp/gensym"
 )
 
 func check(t *testing.T, expected, actual string) {
@@ -20,6 +20,25 @@ func TestMkGen(t *testing.T) {
 	}
 	check(t, "gompsym0", f())
 	check(t, "gompsym1", f())
+}
+
+func TestTwoFuncs(t *testing.T) {
+	const n = 500
+	f1 := MkGen("")
+	f2 := MkGen("")
+	s1 := make([]string, n)
+	s2 := make([]string, n)
+	for i := 0; i < n; i++ {
+		s1[i] = f1()
+	}
+	for i := 0; i < n; i++ {
+		s2[i] = f2()
+	}
+	for i := 0; i < n; i++ {
+		if s1[i] != s2[i] {
+			t.Fatal("Two invocations of MkGen gave different results")
+		}
+	}
 }
 
 func TestSeveralFuncs(t *testing.T) {
