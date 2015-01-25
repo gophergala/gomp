8a5c937eeaf826aed2decc41b3ca0c59b09e3891
Aleksandr Shevchenko <aleksandr.shevchenko@phystech.edu>
2015-01-26 01:45:40 +0400

    Minor changes

--- examples/bench.go
+++ examples/bench.go
@@ -8,15 +8,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Be sure you launch this benchmark via ./run_bench.sh")
+	fmt.Println("Make sure you launch this benchmark via ./run_bench.sh")
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	const N = 100000000
-	c := make([]float64, N+1)
-	d := make([]float64, N+1)
+	c := make([]float64, N)
+	d := make([]float64, N)
 	h := math.Pi / N
 
 	beg1 := time.Now()
-	for i := 0; i <= N; i++ {
+	for i := 0; i < N; i++ {
 		c[i] = math.Exp(math.Sin(float64(i)*h) + math.Cos(math.Pi+float64(i)*h))
 	}
 	time1 := time.Since(beg1)
@@ -24,13 +24,13 @@ func main() {
 
 	beg2 := time.Now()
 	//gomp
-	for i := 0; i <= N; i++ {
+	for i := 0; i < N; i++ {
 		d[i] = math.Exp(math.Sin(float64(i)*h) + math.Cos(math.Pi+float64(i)*h))
 	}
 	time2 := time.Since(beg2)
 	fmt.Println("Parallel execution took: ", time2)
 	fmt.Println("Speedup is ", float64(time1.Nanoseconds())/float64(time2.Nanoseconds()))
-	for i := 0; i <= N; i++ {
+	for i := 0; i < N; i++ {
 		if math.Abs(c[i]-d[i]) > 1e-6 {
 			fmt.Println("Results of computations are not close enough: ", c[i], ", ", d[i])
 			panic("Problems with parallellization")
--- examples/run_bench.sh
+++ examples/run_bench.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gomp < bench.go > bench1.go
-go build bench1.go
-./bench1
+go run bench1.go
+#./bench1
 rm -rf bench1.go bench1
