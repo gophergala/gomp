518e3ff25138b665aecdee85959223712b3254e3
Aleksandr Shevchenko <aleksandr.shevchenko@phystech.edu>
2015-01-26 00:13:43 +0400

    Minor changes on examples

--- examples/bench1.go
+++ examples/bench1.go
@@ -8,28 +8,27 @@ import (
 )
 
 func main() {
-	p := fmt.Println
-	p("Be sure you launch this benchmark via ./run_bench.sh")
-	runtime.GOMAXPROCS(4)
+	fmt.Println("Be sure you launch this benchmark via ./run_bench.sh")
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	const N = 100000000
 	c := make([]float64, N+1)
 	var h float64
-	h = math.Pi / float64(N)
+	h = math.Pi / N
 
-	//Sequential exeqution
+	//Sequential execution
 	beg1 := time.Now()
 	for i := 0; i <= N; i++ {
 		c[i] = math.Exp(math.Sin(float64(i)*h) + math.Cos(math.Pi+float64(i)*h))
 	}
-	end1 := time.Now()
-	p("Sequential execution took: ", end1.Sub(beg1))
+	fmt.Println("Sequential execution took: ", time.Since(beg1))
 
+	//Parallel execution
 	beg2 := time.Now()
 	//gomp
 	for i := 0; i <= N; i++ {
 		c[i] = math.Exp(math.Sin(float64(i)*h) + math.Cos(math.Pi+float64(i)*h))
 	}
 	end2 := time.Now()
-	p("Parallel execution took: ", end2.Sub(beg2))
+	fmt.Println("Parallel execution took: ", end2.Sub(beg2))
 
 }
