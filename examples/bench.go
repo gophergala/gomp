0c48efeb8679ca16fdf5124ab5eaee39c1beb134
Aleksandr Shevchenko <aleksandr.shevchenko@phystech.edu>
2015-01-26 01:22:48 +0400

    Added comparison section

--- examples/bench.go
+++ examples/bench.go
@@ -12,10 +12,9 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	const N = 100000000
 	c := make([]float64, N+1)
-	var h float64
-	h = math.Pi / N
+	d := make([]float64, N+1)
+	h := math.Pi / N
 
-	//Sequential execution
 	beg1 := time.Now()
 	for i := 0; i <= N; i++ {
 		c[i] = math.Exp(math.Sin(float64(i)*h) + math.Cos(math.Pi+float64(i)*h))
@@ -23,14 +22,20 @@ func main() {
 	time1 := time.Since(beg1)
 	fmt.Println("Sequential execution took: ", time1)
 
-	//Parallel execution
 	beg2 := time.Now()
 	//gomp
 	for i := 0; i <= N; i++ {
-		c[i] = math.Exp(math.Sin(float64(i)*h) + math.Cos(math.Pi+float64(i)*h))
+		d[i] = math.Exp(math.Sin(float64(i)*h) + math.Cos(math.Pi+float64(i)*h))
 	}
 	time2 := time.Since(beg2)
 	fmt.Println("Parallel execution took: ", time2)
 	fmt.Println("Speedup is ", float64(time1.Nanoseconds())/float64(time2.Nanoseconds()))
+	for i := 0; i <= N; i++ {
+		if math.Abs(c[i]-d[i]) > 1e-6 {
+			fmt.Println("Results of computations are not close enough: ", c[i], ", ", d[i])
+			panic("Problems with parallellization")
+		}
+	}
+	fmt.Println("Results of computations are close enough")
 
 }
